1f50e4574b7eb195ce31287c814aa5a7b68edf42
Josselin <josselin.abel@eutd.u-picardie.fr>
2020-02-17 16:34:45 +0100

    trouver le pivot

--- main.c
+++ main.c
@@ -51,21 +51,98 @@ void createT(int n, int m, double c[n], double a[m][n], double b[n], double T[n+
     }
 }
 
+int findS(int m, int n, double T[n+m+1][m+1]) { //trouve la colonne du pivot
+    for (int i = 0; i < m+n; i++) {
+        if (T[i][m] > 0) {
+            //printf("findS : %d\n", i);
+            return i;
+        }
+    }
+    return -1;
+}
 
+int findR(int m, int n, int S, double T[n+m+1][m+1]) { //trouve la ligne du pivot
+    int R[m]; //tab des indice des div min
+    double min_res = 300; //res min de la div
+    int nbMin = 0; //nombre de div min
+    int flag = 0; //si colonne base
+    int minI = n+m; //indide min, regle de bland
+    
+    for (int i = 0; i < m; i++) {
+        printf("ligne : %d\n", i);
+        printf("lignes restante : %d\n", m-i-1);
+        if (T[S][i] > 0) { //si la valeur neg, pas pivotable
+            printf("Z Positif : %f\n", T[S][i]);
+            if (T[m+n][i] / T[S][i] < min_res) {
+                min_res = T[m+n][i] / T[S][i]; //on divise la valeur de b par le valeur de la colonne
+                printf("div : %f\n", min_res);
+                for (int j = 0; j < m; j++) {
+                    R[j] = -1;
+                }
+                R[0] = i;
+                nbMin = 1;
+            }else{
+                if (T[m+n][i] / T[S][i] == min_res) {
+                    R[nbMin] = i;
+                    printf("indice min : %d\n", i);
+                    nbMin++;
+                }
+            }
+        }
+    }
+    printf("fin recherche min div\n");
+    if (nbMin > 1) {
+        for (int i = 0; i < nbMin; i++) {
+            for (int j = 0; j < m+n; j++) {
+                if (T[j][R[i]] == 1) {
+                    for (int k = 0; k < m+1 && flag == 0; k++) {
+                        if (T[j][k] != 0 && k != i) {
+                            flag = 1;
+                        }
+                    }
+                    if (flag == 0) {
+                        if (minI > j) {
+                            minI = j;
+                        }
+                    }
+                }
+            }
+        }
+        return minI;
+    }else{
+        printf("R[0] : %d\n", R[0]);
+        return R[0];
+    }
+    
+    return -1;
+}
+
+void pivot(int S, int R, double pivot, double T[n+m+1][m+1]) {
+    
+}
 
 int main(int argc, const char * argv[]) {
     
     double T[n+m+1][m+1];
+    int R = -1, S = -1;
+    double pivot = -1;
     createT(n, m, c, a, b, T);
     
-    
-    
     //affichage du tableau
+    printf("----------Tableau----------\n");
     for (int i = 0; i < m+1; i++) {
         for (int j = 0; j < m+n+1; j++) {
             printf("| %f |", T[j][i]);
         }
         printf("\n");
     }
+    
+    printf("-----------Pivot-----------\n");
+    S = findS(m, n, T);
+    R = findR(m, n, S, T);
+    pivot = T[S][R];
+    
+    printf("S : %d, R : %d, pivot : %f\n", S, R, pivot);
+    
     return 0;
 }
